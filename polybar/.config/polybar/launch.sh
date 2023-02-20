cdbb85b0dacc24e0edc2a2d662a8abac29600c29
Tony Flores Martinez <tony@quimaira.com>
2023-02-20 09:59:02 -0600

    configuracion kubuntu

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -11,7 +11,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar top &
+    MONITOR=$m polybar top --config=$HOME/.config/polybar/config.ini &
   done
 else
   polybar top &
