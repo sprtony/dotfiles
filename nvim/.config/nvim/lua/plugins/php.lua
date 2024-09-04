e0baf5385a7447011c486b529f4d2b38154b6cde
Tony Flores Martinez <pepe_tony93@hotmail.com>
2024-09-04 17:46:06 -0600

    configuracion para omakub y actualizacion de lazyvim

---
+++ nvim/.config/nvim/lua/plugins/php.lua
@@ -0,0 +1,116 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { { "pint" } },
+        blade = { "blade-formatter", "rustywind" },
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.diagnostics.phpstan.with({
+            extra_args = {
+              "--memory-limit=2G",
+            },
+          }),
+        },
+      }
+    end,
+  },
+  {
+    -- Remove phpcs linter.
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = {"phpstan"},
+      },
+    },
+  },
+  {
+    -- Add neotest-pest plugin for running PHP tests.
+    -- A package is also available for PHPUnit if needed.
+    "nvim-neotest/neotest",
+    dependencies = {
+      "V13Axel/neotest-pest",
+      "olimorris/neotest-phpunit"
+    },
+    opts = {
+      adapters = {
+        "neotest-pest",
+        ["neotest-phpunit"] = {
+          filter_dirs = function()
+            return { ".git", "node_modules", "vendor" }
+          end,
+        }
+      }
+    },
+  },
+  {
+    -- Add a Treesitter parser for Laravel Blade to provide Blade syntax highlighting.
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "blade",
+        "php_only",
+      })
+    end,
+    config = function(_, opts)
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+    end,
+  },
+  {
+    -- Add the Laravel.nvim plugin which gives the ability to run Artisan commands
+    -- from Neovim.
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    config = true,
+    opts = {
+      lsp_server = "intelephense",
+      features = { null_ls = { enable = false } },
+    },
+  },
+  {
+    -- Add the blade-nav.nvim plugin which provides Goto File capabilities
+    -- for Blade files.
+    "ricardoramirezr/blade-nav.nvim",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    ft = { "blade", "php" },
+  },
+}
---
+++ ubuntu/.zshrc
@@ -0,0 +1,60 @@
+# Created by Zap installer
+[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+
+# p10k
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+#Mis Variables
+# export _JAVA_AWT_WM_NONREPARENTING=1
+export EDITOR="nvim"
+# export TERM="xterm-256color"
+#export CHROME_EXECUTABLE="google-chrome-stable"
+#export $BROWSER=$CHROME_EXECUTABLE
+# export ANDROID_HOME="$HOME/Programas/android"
+# export JAVA_HOME="$HOME/Programas/openjdk/jdk-11.0.18+10"
+
+#PATH
+export PATH="$HOME/.local/bin:$HOME/bin:/usr/local/bin:$PATH"
+export PATH="$HOME/.config/composer/vendor/bin:$PATH"
+# export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"
+
+export PATH="$HOME/.cargo/env:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+# export PATH="$HOME/.yarn/bin:$PATH"
+# export PATH="$HOME/.bun/bin:$PATH"
+
+
+# export PATH="$HOME/Programas/flutter/bin/:$PATH"
+# export PATH="$HOME/Programas/go/bin:$HOME/go/bin/:$PATH"
+# export PATH="$HOME/Programas/AndroidStudio/bin/:$PATH"
+# export PATH="$ANDROID_HOME/cmdline-tools/tools/bin:$PATH"
+# export PATH="$ANDROID_HOME/emulator:$PATH"
+# export PATH="$ANDROID_HOME/platform-tools:$PATH"
+# export PATH="$JAVA_HOME/bin:$PATH"
+#
+#plugins
+plug "zsh-users/zsh-autosuggestions"
+plug "zap-zsh/supercharge"
+plug "zap-zsh/zap-prompt"
+plug "zsh-users/zsh-syntax-highlighting"
+plug "romkatv/powerlevel10k"
+plug "lukechilds/zsh-nvm"
+plug "hcgraf/zsh-sudo"
+
+#alias
+alias ls="lsd"
+alias cat="bat"
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+
+#load p10k
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+if command -v mise &> /dev/null; then
+  eval "$(mise activate zsh)"
+fi
+
+if command -v zoxide &> /dev/null; then
+  eval "$(zoxide init zsh)"
+fi
