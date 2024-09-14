e6db46a3bc0be8a5bac6a019c7157bf3333af1c8
Jose Antonio Flores Martinez <pepe_tony93@hotmail.com>
2024-09-14 13:45:29 -0600

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,5 @@
+New-alias code "C:\Program Files\VSCodium Insiders\VSCodium - Insiders.exe"
+
 oh-my-posh init pwsh --config 'C:\Users\Quimaira\.config\oh-my-posh\jandedobbeleer.omp.json' | Invoke-Expression
+
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\Quimaira\.config\komorebi'
