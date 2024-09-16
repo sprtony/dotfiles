85d0ddbe355a5ab418a1b51c2b435e715aa6ba02
Tony Flores Martinez <pepe_tony93@hotmail.com>
2024-09-15 20:24:51 -0600

    correccion de carpetas y de variables

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 New-alias code "C:\Program Files\VSCodium Insiders\VSCodium - Insiders.exe"
 
-oh-my-posh init pwsh --config 'C:\Users\Quimaira\.config\oh-my-posh\jandedobbeleer.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config '$env:USERPROFILE\.config\oh-my-posh\jandedobbeleer.omp.json' | Invoke-Expression
 
-$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\Quimaira\.config\komorebi'
+$Env:KOMOREBI_CONFIG_HOME = '$env:USERPROFILE\.config\komorebi'
--- windows/laragon/bin/cmder/vendor/oh-my-posh.lua
+++
@@ -1,1 +0,0 @@
-load(io.popen('oh-my-posh init cmd --config C:/Users/Quimaira/.config/jandedobbeleer.omp.json'):read("*a"))()
